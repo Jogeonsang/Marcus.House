8352fac7ae3a818afc7e879f63298687fcc33ebf
Jogeonsang <rhcn258@gmail.com>
2020-06-22 19:58:15 +0900

    Convert SkeletonUI from JS to TS

--- frontend/src/component/skeletonEl/index.tsx
+++ frontend/src/component/skeletonEl/index.tsx
@@ -1,10 +1,13 @@
 import styled, {css} from "styled-components";
 import React from "react";
 
-const SkeletonLine = styled.div`
+interface ISkeleton{
+    width: string;
+}
+const SkeletonLine = styled.div<ISkeleton>`
   display: inline-block;
   height: 100%;
-  width: ${props => props.width ? props.width : '100%'};
+  width: ${(props: any) => props.width ? props.width : '100%'};
   min-height: 12px;
   background: ${props => css`linear-gradient(-90deg, #F0F0F0 0%, #F8F8F8 50%, #F0F0F0 100%)`};
   background-size: 400% 400%;
