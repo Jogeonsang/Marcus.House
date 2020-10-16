c0826613c865685d866510a752405dc2dccce3e6
marcus <rhcn258@gmail.com>
2020-10-16 16:48:02 +0900

    git rm -r db, config info

--- backend/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    user : 'marcus',
-    host : 'localhost',
-    database : 'marcus_house_db',
-    password : 'jung781',
-    port : 5432,
-}
--- backend/db.js
+++
@@ -1,4 +0,0 @@
-const pgp = require('pg-promise')(/!* options *!/)
-const db = pgp('postgres://marcus:jung781@localhost:5432/marcus_house_db')
-
-module.exports = db
